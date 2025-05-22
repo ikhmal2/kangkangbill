c24b90f5c475b1fc0deb5d117eb31e9731eeb214
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-05-22 22:20:16 +0800

    feat: new page for groups

---
+++ components/GroupItem.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div class="flex justify-between">
+			<UIcon name="material-symbols:grid-on-outline-sharp" class="size-8" />
+			<div>{{ groupName }}</div>
+			<div class="text-green-600 text-xs">you are owed RM 112.35</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	groupName: String
+})
+</script>
+
+<style scoped></style>
---
+++ pages/Groups.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<OwedAmount :total-amount="totalAmountOwed" />
+		<GroupItem v-for="i in groupList" :group-name="i" />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { FriendList as list, groupList } from '~/assets/utils/mock';
+
+const totalAmountOwed = computed(() => {
+	let amount = 0;
+	list.forEach(item => {
+		amount += item.amount;
+	})
+	return amount;
+})
+</script>
+
+<style scoped></style>
