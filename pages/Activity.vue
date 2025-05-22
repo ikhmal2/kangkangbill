e342d2f8e484f0affb18e8dbe751182dd247603c
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-05-22 22:20:33 +0800

    feat: add  placeholders for new pages

---
+++ pages/Activity.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		Activity
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ pages/AddExpense.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		Add Expense
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
