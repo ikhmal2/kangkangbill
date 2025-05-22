868bf1c645c910cee098ada64e30be8431f3617d
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-05-22 22:18:32 +0800

    feat: add mock data

---
+++ assets/utils/mock.ts
@@ -0,0 +1,25 @@
+type borrower = {
+  name: string;
+  amount: number;
+};
+
+export const FriendList: borrower[] = [
+  {
+    name: "Ijud",
+    amount: 33.75,
+  },
+  {
+    name: "Alif Aiman",
+    amount: 43.1,
+  },
+  {
+    name: "Ariff",
+    amount: 78.6,
+  },
+  {
+    name: "Harith",
+    amount: 0,
+  },
+];
+
+export const groupList = ["Loy Datang"];
